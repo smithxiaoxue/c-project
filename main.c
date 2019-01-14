0f5a91e3675f408c10442ca786f55c47f9353a37
zhangxueying <ying@qq.com>
2019-01-14 14:04:30 +0800

    取钱功能

--- main.c
+++ main.c
@@ -13,7 +13,37 @@ int main()
         printf("1--退出系统"\n);
         printf("请输入功能编号"\n);
         scanf("%d", $a);
-    }
+        if (a == 1)
+        {
+            int cun;
+            char x;
+            printf("进入存钱功能，请输入要存入的金额\n");
+            scanf("%d", &cun);
+            qian += cun;
+            printf("存入成功，点击回车继续\n");
+            scanf("%c" & x);
+            scanf("%c" & x);
+        }
+        if (a == 2)
+        {
+            int qu;
+            char x;
+            printf("进入取钱功能，请输入要取的金额\n");
+            scanf("%d" qu);
+            if (qian >= qu)
+            {
+                qian -= qu;
+                printf("取款成功，点击回车继续\n");
+                scanf("%c" & x);
+                scanf("%c" & x);
+            }
+            else
+            {
+                printf("余额不足，取款失败，点击回车继续\n");
+                scanf("%c" & x);
+                scanf("%c" & x);
+            }
+        }
 
-    return 0;
-}
+        return 0;
+    }
