c3d26ab92b45e87f0450ba4082f3ab3d2ca9143a
zhangxueying <ying@qq.com>
2019-01-14 11:34:49 +0800

    第一次

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int qian;
+    printf("进入存钱系统\n");
+    while (1)
+    {
+        int a = 0;
+        printf("1--存钱"\n);
+        printf("1--取钱"\n);
+        printf("1--显示金额"\n);
+        printf("1--退出系统"\n);
+        printf("请输入功能编号"\n);
+        scanf("%d", $a);
+    }
+
+    return 0;
+}
